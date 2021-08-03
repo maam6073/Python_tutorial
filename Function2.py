bf08d25e012cc7e53db60454b2cc3044be91c04d
DESKTOP-35HGFA2\bitcamp <maam10@naver.com>
2021-08-03 17:47:32 +0900

    함수2

--- Function2.py
+++ Function2.py
@@ -14,3 +14,33 @@ def func2(a):
 
 #호출
 print(func2(1))
+
+#불변형식을 내부에서 읽기+쓰기
+g = 1
+def testScope(a):
+    global g
+    g = 2
+    return g+a
+#호출
+print(testScope(1))
+print("전역변수 g:",g)
+
+#기본값을 셋팅
+def times(a=10,b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(a=5))
+print(times(3,4))
+
+#키워드 인자(파라미터명을 명시)
+def connectURI(sever,port):
+    strURL = "http://" + server +":"+port
+    return strURL
+
+#호출
+print(connectURI("bitCamp","80"))
+
+
+
