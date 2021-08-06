a2446b3859e9fda033a34be266e39d729ad4a9ae
DESKTOP-35HGFA2\bitcamp <maam10@naver.com>
2021-08-06 11:38:09 +0900

    데이터베이스2

---
+++ db2.py
@@ -0,0 +1,31 @@
+#db2.py
+import sqlite3
+#연결객체 생성(파일에 영구적으로 기록)
+con = sqlite3.connect("c:\\work\\sample.db")
+
+#구문을 수행할 커서 객체를 생성
+cur = con.cursor()
+#데이터를 담을 테이블을 생성
+cur.execute("create table PhoneBook (name text, phoneNum text);")
+#1건을 입력
+cur.execute("insert into PhoneBook values ('derick','010-111');")
+#입력 파라미터 처리
+name = "gildong"
+phoneNumber ="010-222"
+cur.execute("insert into PhoneBook values (?,?);",(name,phoneNumber))
+#다중의 레코드(데이터를 입력하는 경우)
+datalist =(("tom","010-123"),("dsp","010-454"))
+cur.excutemany("insert into PhoneBook values(?,?);",datalist)
+
+#결과를 검색
+cur.execute("select * from PhoneBook;")
+print("---fetchone()---")
+print( cur.fetchone() )
+print("---fetchmany(2)---")
+print(cur.fetchmany(2))
+print("---fetchall()---")
+cur.execute("select * from PhoneBook;")
+print(cur.fetchall())
+
+#정상적으로 작업을 완료
+con.commit()
